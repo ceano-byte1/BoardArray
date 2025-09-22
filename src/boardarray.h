fc8066b7b710fd46550b534355f726570ae4019b
ceano-byte1 <criseanneymar.contreras@cit.edu>
2025-09-22 08:43:09 +0800

    boardArray ans

--- src/boardarray.h
+++ src/boardarray.h
@@ -14,9 +14,34 @@ class BoardArray : public Board {
         }
 
         void add(Entry* entry) {
-            // TODO: IMPLEMENT THIS FUNCTION
-            // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
-            return;
+            if (index == 0) {
+                array[0] = *entry;
+                index++;
+                return;
+            }
+
+            if (index == SIZE && !entry->compare(array[index - 1])) {
+                cout << entry->name << "'s score is too low to be added!" << endl;
+                return;
+            }
+
+            int pos = index;
+            for (int i = 0; i < index; i++) {
+                if (entry->compare(array[i])) {
+                    pos = i;
+                    break;
+                }
+            }
+
+            if (index < SIZE) {
+                index++;
+            }
+
+            for (int j = index - 1; j > pos; j--) {
+                array[j] = array[j - 1];
+            }
+
+            array[pos] = *entry;
         }
 
         void print() {
