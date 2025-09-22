72894567d58e21c5e98e1b7ffdc44bec7247a565
ceano-byte1 <criseanneymar.contreras@cit.edu>
2025-09-22 08:53:47 +0800

    answer

--- src/boardarray.h
+++ src/boardarray.h
@@ -14,35 +14,35 @@ class BoardArray : public Board {
         }
 
         void add(Entry* entry) {
-            if (index == 0) {
-                array[0] = *entry;
-                index++;
-                return;
-            }
+    if (index == 0) {
+        array[0] = *entry;
+        index++;
+        return;
+    }
 
-            if (index == SIZE && !entry->compare(array[index - 1])) {
-                cout << entry->name << "'s score is too low to be added!" << endl;
-                return;
-            }
+    if (index == SIZE && !entry->compare(&array[index - 1])) {
+        cout << entry->name << "'s score is too low to be added!" << endl;
+        return;
+    }
 
-            int pos = index;
-            for (int i = 0; i < index; i++) {
-                if (entry->compare(array[i])) {
-                    pos = i;
-                    break;
-                }
-            }
-
-            if (index < SIZE) {
-                index++;
-            }
-
-            for (int j = index - 1; j > pos; j--) {
-                array[j] = array[j - 1];
-            }
-
-            array[pos] = *entry;
+    int pos = index;
+    for (int i = 0; i < index; i++) {
+        if (entry->compare(&array[i])) {
+            pos = i;
+            break;
         }
+    }
+
+    if (index < SIZE) {
+        index++;
+    }
+
+    for (int j = index - 1; j > pos; j--) {
+        array[j] = array[j - 1];
+    }
+
+    array[pos] = *entry;
+}
 
         void print() {
             for (int i = 0; i < index; i++) {
